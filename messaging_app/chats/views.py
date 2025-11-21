d1561db0a2a6cc55005a6fb8fee953529b538802
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-21 19:03:37 +0200

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,1 +1,15 @@
+from rest_framework import viewsets
+from .models import Message
+from .serializers import MessageSerializer
+from .permissions import IsParticipantOfConversation
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        )
 
