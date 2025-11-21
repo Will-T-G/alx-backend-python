6f3572961a1c28005cbe7fd0dba98fe6b43415b4
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-21 19:35:02 +0200

    Update filters.py

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,1 +1,19 @@
+class MessageFilter(django_filters.FilterSet):
+    # Filter messages sent after a time
+    created_after = django_filters.DateTimeFilter(
+        field_name="created_at", lookup_expr="gte"
+    )
 
+    # Filter messages sent before a time
+    created_before = django_filters.DateTimeFilter(
+        field_name="created_at", lookup_expr="lte"
+    )
+
+    # Filter messages from a specific user
+    user_id = django_filters.NumberFilter(
+        field_name="user__id", lookup_expr="exact"
+    )
+
+    class Meta:
+        model = Message
+        fields = ["user_id", "created_after", "created_before"]
