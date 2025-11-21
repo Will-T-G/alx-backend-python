fadf05aed62545335a220fe8115f1952ccd120e6
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-21 19:01:52 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,32 @@
 from rest_framework import permissions
+from rest_framework import permissions
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allow access only if:
+    - user is authenticated
+    - user is a participant in the conversation of the object
+    """
+
+    def has_permission(self, request, view):
+        # User must be logged in
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        """
+        obj can be:
+        - a Conversation instance (with participants ManyToMany)
+        - a Message instance (with message.conversation)
+        """
+        # Case 1: obj *is* a Conversation
+        if hasattr(obj, "participants"):
+            return request.user in obj.participants.all()
+
+        # Case 2: obj *is* a Message that belongs to a conversation
+        if hasattr(obj, "conversation"):
+            return request.user in obj.conversation.participants.all()
+
+        return False
 
 
 class IsOwner(permissions.BasePermission):
