65b3d425eaf177f973910c9f3424096384b6a024
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-21 18:36:24 +0200

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,36 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector import Error
+
+
+def stream_users():
+    """
+    Generator function that connects to the ALX_prodev database
+    and yields rows from the user_data table one by one.
+    """
+    try:
+        # Connect to the database
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',             
+            password='####',
+            database='ALX_prodev'
+        )
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data;")
+
+        #  Yield each row one at a time
+        for row in cursor:
+            yield row
+
+    except Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+
 
