0dab49432abe799a03b8904ec33eb6525d92b258
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-30 17:21:52 +0200

    Update tests.py

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,1 +1,24 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from messaging.models import Message, Notification
+
+
+class MessageSignalTest(TestCase):
+    def setUp(self):
+        self.sender = User.objects.create_user(username="sender", password="pass123")
+        self.receiver = User.objects.create_user(username="receiver", password="pass123")
+
+    def test_notification_created_on_message(self):
+        msg = Message.objects.create(
+            sender=self.sender,
+            receiver=self.receiver,
+            content="Hello there!"
+        )
+
+        notifications = Notification.objects.filter(user=self.receiver)
+        self.assertEqual(notifications.count(), 1)
+
+        notif = notifications.first()
+        self.assertEqual(notif.message, msg)
+        self.assertFalse(notif.is_read)
 
